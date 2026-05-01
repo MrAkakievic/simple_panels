c0e693cc45e949e0aef374c91795a996d9b5da3b
Akaky <212865037+MrAkakievic@users.noreply.github.com>
2026-05-01 17:45:20 -0400

    English update, rearrangement of the lines of the code.  It's only until  the 146 line

--- simple_panels.py
+++ simple_panels.py
@@ -1,4 +1,24 @@
 
+# Features should be added in future:
+"""
+1). designes   # Shows designes you can use just now without making your own ones
+2). default_style   # Constructor where you can make/choose designes for different classes or establish a built-in design pack
+"""
+
+
+
+
+
+######################################################################################
+
+default_size_of_display = (1600, 900)
+
+######################################################################################
+
+
+
+
+
 import pygame, types
 pygame.init()
 
@@ -21,18 +41,6 @@ class AutoTypeHere:
 
 
 
-
-
-# Features you should add:
-"""
-1). designes   # Shows designes you can use just now without making your own ones
-2). default_style   # Constructor where you can make/choose designes for different classes or establish a built-in design pack
-"""
-
-
-
-
-
 display_for_sps = None
 display_size = None
 nolimits = NoLimitsTypeHere()
@@ -44,9 +52,9 @@ def select_display(*display):
     global display_for_sps, display_size
     if len(display) == 0:
         if display_for_sps is not None:
-            raise DisplayError('Display already initialized, cannot create new one')
-        display_for_sps = pygame.display.set_mode((1600, 900))
-        display_size = (1600, 900)
+            raise DisplayError('Display already initialized, cannot create a new one')
+        display_for_sps = pygame.display.set_mode(default_size_of_display)   # Established the display with default simple_panels size
+        display_size = default_size_of_display
     elif len(display) == 1:
         if isinstance(display[0], pygame.Surface):
             display_for_sps = display[0]
@@ -58,10 +66,10 @@ def select_display(*display):
 
 
 
-class nummer:
+class nummer:   # The panel fixed on a some place that shows some text when user interacts with some simple_panels object
     exist = False
-    coordinates = None
-    width = None
+    coordinates = None   # Top-left point
+    width = None   # The same value for width along x and width along y
     font = None
 
     @classmethod
@@ -71,15 +79,15 @@ class nummer:
         if type(coordinates) is not tuple and type(coordinates) is not list and type(coordinates):
             raise TypeError(f'The {type(coordinates)} type had passed, but a tuple or list expected')
         if type(width) is not int and type(width) is not AutoTypeHere:
-            raise TypeError(f'The {type(width)} type had passed, but int expected')
+            raise TypeError(f'The {type(width)} type had passed, but int was expected')
         if len(coordinates) != 2:
-            raise ValueError(f'The {len(coordinates)} arguments had passed, but 2 ones (x, y) was expected')
+            raise ValueError(f'{len(coordinates)} arguments had passed, but 2 ones (x, y) was expected')
         for i in range(2):
             if type(coordinates[i]) is not int:
                 raise TypeError(f'The {type(coordinates)} type was passed for {i} position in the coordinates, but int was expected')
         if type(width) is AutoTypeHere:
             if display_size[0] - coordinates[0] < 15 or display_size[1] - coordinates[1] < 15:
-                raise ValueError(f'Width of a nummer cannot be smaller that 15, but coordinated don\'t allowing fitting into the so little space were passed')
+                raise ValueError(f'The width of a nummer can\'t be smaller that 15. The coordinatates passed don\'t fit within the remaining space up to the boundaries of the display')
             else:
                 io = display_size[0] - coordinates[0]
                 if display_size[1] - coordinates[1] < io:
@@ -89,23 +97,42 @@ class nummer:
             if width < 15:
                 raise ValueError(f'{width} is too small value. You can only use 15 and bigger')
         if not (type(design).__qualname__.split('.')[0] == 'design'):
-            raise TypeError(f'The design type was expected, but {type(design)} had passed')
+            raise TypeError(f'The design type was expected, but {type(design)} had passed')   # These is a design type for every class of interactive object but all of them located in the design class
         elif len(type(design).__qualname__.split('.')) == 1:
             raise TypeError('You have passed the design class, but it\'s must be a class object')
         elif len(type(design).__qualname__.split('.')) > 2:
             raise TypeError(f'The design type was expected, but {type(design)} had passed')
         elif not (type(design).__qualname__.split('.')[1] == 'NummerDesign'):
-            raise DesignError('You have passed the design, which doesn\'t designed for nummer. Check how you created this')
+            raise DesignError('You have passed the design, which doesn\'t designed for nummer. Check how you was created it')
         cls.exist = True
         cls.coordinates = tuple(coordinates)
         cls.font = pygame.font.SysFont(design.font_type, design.font_size)
         cls.font_color = design.font_color
         cls.width = width
         cls.border_radius = design.border_radius
-
         cls.carcas = pygame.surface.Surface((cls.width, cls.width), pygame.SRCALPHA)
+
+
+
+
+
+
+
+
+
+
+        # Some testing code
+
         pygame.draw.rect(cls.carcas, design.inside_color, (0, 0, width, width), border_radius = design.border_radius)
         pygame.draw.rect(cls.carcas, design.edges_color, (0, 0, width, width), 5, design.border_radius)
+
+
+
+
+
+
+
+
 
         
     @classmethod
