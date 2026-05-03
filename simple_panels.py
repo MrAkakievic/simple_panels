b55e58e1c00e11d68ed6aeda8a0b695e69564dd1
Akaky <212865037+MrAkakievic@users.noreply.github.com>
2026-05-03 14:19:47 -0400

    Some English mistakes removed, comments added and some different corrections a bit

--- simple_panels.py
+++ simple_panels.py
@@ -20,6 +20,7 @@ default_size_of_display = (1600, 900)
 
 
 import pygame, types
+from typing import Union
 pygame.init()
 
 class DisplayError(Exception):
@@ -33,18 +34,18 @@ class DesignError(Exception):
 
 
 
-class NoLimitsTypeHere:
+class nolimits:
     2 + 2 == 4
 
-class AutoTypeHere:
+class auto:
     2 + 2 == 4
 
 
 
 display_for_sps = None
 display_size = None
-nolimits = NoLimitsTypeHere()
-auto = AutoTypeHere()
+nolimits = nolimits()
+auto = auto()
 
 
 
@@ -60,7 +61,7 @@ def select_display(*display):
             display_for_sps = display[0]
             display_size = display[0].get_size()
         else:
-            raise DisplayError(f'select_display() function requires a pygame.Surface object, but {type(display[0])} ones have passed')
+            raise DisplayError(f'select_display() function requires a pygame.Surface object, but {type(display[0])} have passed')
     else:
         raise DisplayError(f'select_display() function requires one argument, but {len(display)} ones have passed')
 
@@ -69,23 +70,23 @@ def select_display(*display):
 class nummer:   # The panel fixed on a some place that shows some text when user interacts with some simple_panels object
     exist = False
     coordinates = None   # Top-left point
-    width = None   # The same value for width along x and width along y
+    width = None   # The same value for width along x and for width along y
     font = None
 
     @classmethod
-    def set(cls, coordinates, width, design):
+    def set(cls, coordinates: tuple | list, width: Union[int, auto], design):
         if not display_size:
             raise DisplayError('Display wasn\'t detected')
         if type(coordinates) is not tuple and type(coordinates) is not list and type(coordinates):
             raise TypeError(f'The {type(coordinates)} type had passed, but a tuple or list expected')
-        if type(width) is not int and type(width) is not AutoTypeHere:
+        if type(width) is not int and type(width) is not auto:
             raise TypeError(f'The {type(width)} type had passed, but int was expected')
         if len(coordinates) != 2:
             raise ValueError(f'{len(coordinates)} arguments had passed, but 2 ones (x, y) was expected')
         for i in range(2):
             if type(coordinates[i]) is not int:
                 raise TypeError(f'The {type(coordinates)} type was passed for {i} position in the coordinates, but int was expected')
-        if type(width) is AutoTypeHere:
+        if type(width) is auto:
             if display_size[0] - coordinates[0] < 15 or display_size[1] - coordinates[1] < 15:
                 raise ValueError(f'The width of a nummer can\'t be smaller that 15. The coordinatates passed don\'t fit within the remaining space up to the boundaries of the display')
             else:
@@ -111,32 +112,11 @@ class nummer:   # The panel fixed on a some place that shows some text when user
         cls.width = width
         cls.border_radius = design.border_radius
         cls.carcas = pygame.surface.Surface((cls.width, cls.width), pygame.SRCALPHA)
-
-
-
-
-
-
-
-
-
-
-        # Some testing code
-
         pygame.draw.rect(cls.carcas, design.inside_color, (0, 0, width, width), border_radius = design.border_radius)
         pygame.draw.rect(cls.carcas, design.edges_color, (0, 0, width, width), 5, design.border_radius)
-
-
-
-
-
-
-
-
-
-        
+    
     @classmethod
-    def pict(cls, text):
+    def pict(cls, text):   # Very function nummer exists to implement. Any interactive element can show some text it wants over nummer.pict()
         if not display_size:
             raise DisplayError('simple_panels is not seen the display')
         display_for_sps.blit(cls.carcas, (cls.coordinates[0], cls.coordinates[1]))
@@ -145,17 +125,19 @@ class nummer:   # The panel fixed on a some place that shows some text when user
             
 
 
-class design:
+class design:   # The general class containing designes for all types of intecactive objects. Exists only for containing different types of designes, it's planned that a user thinks only one class for design exists
+    # The different design classes creates via @classmethod functions for a user, not via constructors
+    
     @classmethod
-    def slider(cls, inside_color = (255, 0, 0), edges_color = (0, 0, 0), separators_exist = None, notes_exist = None, /):
+    def slider(cls, inside_color: tuple[int, int, int] | list[int, int, int] = (255, 0, 0), edges_color: tuple[int, int, int] | list[int, int, int] = (0, 0, 0), separators_exist: tuple[int, int, int] | list[int, int, int] = None, notes_exist: bool = None, /):
         return design.SliderDesign(inside_color, edges_color, separators_exist, notes_exist)
     
     @classmethod
-    def nummer(cls, border_radius = 0, inside_color = (255, 0, 0), edges_color = (0, 0, 0), font_color = (255, 255, 255), font_size = 16, font_type = 'centurygothic'):
+    def nummer(cls, border_radius: int = 0, inside_color: tuple[int, int, int] | list[int, int, int] = (255, 0, 0), edges_color: tuple[int, int, int] | list[int, int, int] = (0, 0, 0), font_color: tuple[int, int, int] | list[int, int, int] = (255, 255, 255), font_size: int = 16, font_type: str = 'centurygothic'):
         return design.NummerDesign(border_radius, inside_color, edges_color, font_color, font_size, font_type)
     
     @classmethod
-    def button(self, text, border_radius = 0, color_doesnt_downed = (255, 0, 0), color_downed = (255, 125, 125), text_color_doesnt_downed = (255, 255, 255), text_color_downed = (125, 0, 0), font_type = ('centurygothic'), font_size = 16, edges_color = (0, 0, 0), change_time_ticks = 0):
+    def button(cls, text, border_radius: int = 0, color_doesnt_downed: tuple[int, int, int] | list[int, int, int] = (255, 0, 0), color_downed: tuple[int, int, int] | list[int, int, int] = (255, 125, 125), text_color_doesnt_downed: tuple[int, int, int] | list[int, int, int] = (255, 255, 255), text_color_downed: tuple[int, int, int] | list[int, int, int] = (125, 0, 0), font_type: str = ('centurygothic'), font_size: int = 16, edges_color: tuple[int, int, int] | list[int, int, int] = (0, 0, 0), change_time_ticks: int = 0):
         return design.ButtonDesign(text, border_radius, color_doesnt_downed, color_downed, text_color_doesnt_downed, text_color_downed, font_type, font_size, edges_color, change_time_ticks)
 
     class SliderDesign:
@@ -221,7 +203,7 @@ class design:
             self.font_type = font_type
     
     class ButtonDesign:
-        def __init__(self, text, border_radius, color_doesnt_downed, color_downed, text_color_doesnt_downed, text_color_downed, font_type, font_size, edges_color, change_time_ticks):
+        def __init__(self, text, border_radius, color_doesnt_downed, color_downed, text_color_doesnt_downed, text_color_downed, font_type, font_size, edges_color, change_time_ticks):   # change_time_ticks is how long time animation of pressing a button and animation of releasing of it play
             try:
                 text = str(text)
             except:
@@ -303,8 +285,8 @@ class design:
 
 
 
-class RelateSlider:
-    def __init__(self, dependent_variable_as_list: list, coordinates: tuple | list, min_value: int | float, max_value: int | float, k_steps: int, live_level: int, design):
+class RelateSlider:   # The type of slider in which the value it represents varyes between tips-values, and middle points-values are between that two tip values. Like here: *1* 2 3 4 *5* when 1 pinned as a start point and 5 pinned as an end point
+    def __init__(self, dependent_variable_as_list: list, coordinates: tuple | list, min_value: int | float, max_value: int | float, k_steps: int, live_level: int, design):   # dependent_variable_as_list is a list of 1 element   # live_level is a point where the slider on is in the start of a simulation
         if type(min_value) is not int and type(min_value) is not float:
             raise TypeError(f'Int or float type was expected, but {type(min_value)} had passed')
         if type(max_value) is not int and type(max_value) is not float:
@@ -337,9 +319,17 @@ class RelateSlider:
         else:
             if live_level > k_steps or live_level < 0:
                 raise ValueError(f'Had passed the impossible live_level. It\'s the {live_level}, but a one from 0 to {k_steps} was expected')
-#        if type(design) is not SliderDesign:
-#            raise ValueError(f'A design type object has passed, which doesn\'t intended for the Sliders. Check whereby it was created')
-        values_tuple = []
+
+        if not (type(design).__qualname__.split('.')[0] == 'design'):
+            raise TypeError(f'The design type was expected, but {type(design)} had passed')
+        elif len(type(design).__qualname__.split('.')) == 1:
+            raise TypeError('You have passed the design class, but it\'s must be a class object')
+        elif len(type(design).__qualname__.split('.')) > 2:
+            raise TypeError(f'The design type was expected, but {type(design)} had passed')
+        elif not (type(design).__qualname__.split('.')[1] == 'SliderDesign'):
+            raise DesignError('A design type object has passed, which doesn\'t intended for the Sliders. Check whereby it was created')
+
+        values_tuple = []   # values at all the points
         one_step = (max_value - min_value) / k_steps
         for i in range(k_steps):
             values_tuple.append(min_value + one_step * i)
@@ -348,22 +338,25 @@ class RelateSlider:
         self.design = design
         self.coordinates = coordinates
         self.level = live_level
-        self.xchunk = round(coordinates[2] / k_steps)
+        self.xchunk = round(coordinates[2] / k_steps)   # distance between 2 neighboring points
         self.dependent_variable_as_list = dependent_variable_as_list
         
     def pict(self):
         pass
 
-class Incut:
+class Incut:   # A interactive thing pops up when some interctive object requests some input
     def create():
         pass
     
 class TapButton:
-    def __init__(self, rect, function, design):
+    def __init__(self, rect: tuple[int, int, int, int] | list[int, int, int, int], function: types.FunctionType, design):   # rect is a rect within which the button fits
         if type(rect) is not tuple and type(rect) is not list:
             raise TypeError(f'rect argument must be a list or tuple type')
         elif len(rect) != 4:
             raise ValueError(f'rect require 4 arguments (x, y, width, height), but {len(rect)} had passed')
+        for i in range(len(rect)):
+            if type(rect[i]) is not int:
+                raise TypeError(f'The {rect[i]}th element of coordinates is not int')
         if type(function) is not types.FunctionType:
             raise TypeError(f'function type was expected, but {type(function)} had passed')
         if not (type(design).__qualname__.split('.')[0] == 'design'):
@@ -373,7 +366,7 @@ class TapButton:
         elif len(type(design).__qualname__.split('.')) > 2:
             raise TypeError(f'The design type was expected, but {type(design)} had passed')
         elif not (type(design).__qualname__.split('.')[1] == 'ButtonDesign'):
-            raise DesignError('You have passed the design, which doesn\'t designed for button. Check how you created this')
+            raise DesignError('A design type object has passed, which doesn\'t intended for the Buttons. Check whereby it was created')
         self.rect = rect
         self.function = function
         self.border_radius = design.border_radius
